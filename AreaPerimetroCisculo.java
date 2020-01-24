54438df30ad3490ee620a751870060b8564ea75c
juandValencia <54083618+juandValencia@users.noreply.github.com>
2020-01-24 06:29:08 -0500

    Add files via upload

---
+++ AreaPerimetroCisculo.java
@@ -0,0 +1,16 @@
+//calcular el area y el perimetro de una circunderencia con su radio 
+class AreaPerimetroCirculo{
+	public static void main (String args[]){
+		double Radio , Area ,Perimetro ;
+		Radio=28;
+		Area=3.1416*(Radio*Radio);
+		Perimetro=2*3.1416*Radio;
+			System.out.println("el area del circulo cuyo radio es 28  es "+Area);
+			System.out.println(" y el perimetro  es "+Perimetro); 
+		}
+	}
+		
+		
+		
+	
+
