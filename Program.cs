9c058082129dc71d91f147adeb198f5988ee09a0
juandValencia <54083618+juandValencia@users.noreply.github.com>
2020-02-05 06:37:48 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un texto");
+            String texto;
+            texto = Console.ReadLine();
+            int a=Int32.Parse(texto);
+            int b;
+            Console.Write(a);
+            while (a > 0) {
+                b = a % 10;
+
+                a = a / 10;
+
+                Console.Write(b);
+            }
+            Console.Read();
+
+        }
+    }
+}
